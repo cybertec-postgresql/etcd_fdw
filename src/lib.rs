de5311260f8429fba01a1c4884a4b78c996f9234
if-loop69420 <j.sztavi@pm.me>
2025-09-16 16:02:16 +0200

    fix: Fix most of the errors in the build. Now only the weird __pgrx_marker () build error is left.

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,41 @@
+use etcd_client::Client;
 use futures::Stream;
-use pgrx::prelude::*;
+use pgrx::pg_sys::panic::ErrorReport;
+use pgrx::PgSqlErrorCode;
 use supabase_wrappers::prelude::*;
-use tokio::runtime::Runtime;
-
-::pgrx::pg_module_magic!(name, version);
+use tokio::runtime::*;
 
 #[wrappers_fdw(
-    version = "0.0.0",
+    version = "0.0.1",
     author = "Cybertec PostgreSQL International GmbH",
     error_type = "EtcdFdwError"
 )]
-pub struct EtcdFdw {
+pub(crate) struct EtcdFdw {
     client: Client,
     rt: Runtime,
 }
 
 pub enum EtcdFdwError {}
 
-impl ForeignDataWrapper<EtcdFdw> for EtcdFdw {
-    fn new(server: ForeignServer) -> Result<Self, EtcdFdwError> {
-        // Open connection to etcd specified through the server parameter
+impl From<EtcdFdwError> for ErrorReport {
+    fn from(_value: EtcdFdwError) -> Self {
+        ErrorReport::new(PgSqlErrorCode::ERRCODE_FDW_ERROR, "", "")
+    }
+}
 
-        let client =
-            Client::new([server.server_name], None).expect("Couldn't connect to etcd server");
+type EtcdFdwResult<T> = std::result::Result<T, EtcdFdwError>;
+
+impl ForeignDataWrapper<EtcdFdwError> for EtcdFdw {
+    fn new(server: ForeignServer) -> Result<EtcdFdw, EtcdFdwError> {
+        // Open connection to etcd specified through the server parameter
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let client = rt
+            .block_on(Client::connect(&[server.server_name], None))
+            .unwrap();
 
         // Perform health checks and throw error on unhealthy
 
-        Self { client }
+        Ok(Self { client, rt })
     }
 
     fn begin_scan(
@@ -36,34 +45,34 @@ impl ForeignDataWrapper<EtcdFdw> for EtcdFdw {
         sorts: &[Sort],
         limit: &Option<Limit>,
         options: &std::collections::HashMap<String, String>,
-    ) -> Result<(), EtcdFdw> {
+    ) -> Result<(), EtcdFdwError> {
         todo!()
     }
 
-    fn iter_scan(&mut self, row: &mut Row) -> Result<Option<()>, EtcdFdw> {
+    fn iter_scan(&mut self, row: &mut Row) -> Result<Option<()>, EtcdFdwError> {
         todo!()
     }
 
-    fn end_scan(&mut self) -> Result<(), EtcdFdw> {
+    fn end_scan(&mut self) -> Result<(), EtcdFdwError> {
         todo!()
     }
 
     fn begin_modify(
         &mut self,
         _options: &std::collections::HashMap<String, String>,
-    ) -> Result<(), EtcdFdw> {
+    ) -> Result<(), EtcdFdwError> {
         todo!()
     }
 
-    fn insert(&mut self, _row: &Row) -> Result<(), EtcdFdw> {
+    fn insert(&mut self, _row: &Row) -> Result<(), EtcdFdwError> {
         todo!()
     }
 
-    fn update(&mut self, _rowid: &Cell, _new_row: &Row) -> Result<(), EtcdFdw> {
+    fn update(&mut self, _rowid: &Cell, _new_row: &Row) -> Result<(), EtcdFdwError> {
         todo!()
     }
 
-    fn delete(&mut self, _rowid: &Cell) -> Result<(), EtcdFdw> {
+    fn delete(&mut self, _rowid: &Cell) -> Result<(), EtcdFdwError> {
         todo!()
     }
 
@@ -74,11 +83,11 @@ impl ForeignDataWrapper<EtcdFdw> for EtcdFdw {
         _sorts: &[Sort],
         _limit: &Option<Limit>,
         _options: &std::collections::HashMap<String, String>,
-    ) -> Result<(i64, i32), EtcdFdw> {
+    ) -> Result<(i64, i32), EtcdFdwError> {
         todo!()
     }
 
-    fn end_modify(&mut self) -> Result<(), EtcdFdw> {
+    fn end_modify(&mut self) -> Result<(), EtcdFdwError> {
         todo!()
     }
 }
