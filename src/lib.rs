609d4e3d9288ce1bf4164d11c8d81148555b1baa
Jeremy Sztavinovszki <81078181+if-loop69420@users.noreply.github.com>
2025-09-17 09:19:56 +0000

    Implement core features needed to read from etcd (#10) (fixing all mentioned in #1)
    * feat: implement the new function. Also started implementing the begin_scan and iter_scan functions
    
    * feat: Add basic implementation of end_scan
    
    * feat,fix: Move connstr into a passable option. Add description for todos. Comment out get_rel_size. Test a select and make sure this works. Rename everything to etcd_fdw
    
    * docs: Add simple docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
-use etcd_client::Client;
+use etcd_client::{Client, GetOptions, KeyValue};
 use futures::Stream;
 use pgrx::pg_sys::panic::ErrorReport;
+use pgrx::pg_sys::ErrorContextCallback;
 use pgrx::PgSqlErrorCode;
 use supabase_wrappers::prelude::*;
+use thiserror::Error;
 use tokio::runtime::*;
 
 pgrx::pg_module_magic!();
@@ -15,9 +17,21 @@ pgrx::pg_module_magic!();
 pub(crate) struct EtcdFdw {
     client: Client,
     rt: Runtime,
+    prefix: String,
+    fetch_results: Vec<KeyValue>,
 }
 
-pub enum EtcdFdwError {}
+#[derive(Error, Debug)]
+pub enum EtcdFdwError {
+    #[error("Failed to fetch from etcd")]
+    FetchError(String),
+
+    #[error("Failed to connect to client")]
+    ClientConnectionError(String),
+
+    #[error("No connection string option was specified. Specify it with connstr")]
+    NoConnStr(()),
+}
 
 impl From<EtcdFdwError> for ErrorReport {
     fn from(_value: EtcdFdwError) -> Self {
@@ -28,66 +42,114 @@ impl From<EtcdFdwError> for ErrorReport {
 type EtcdFdwResult<T> = std::result::Result<T, EtcdFdwError>;
 
 impl ForeignDataWrapper<EtcdFdwError> for EtcdFdw {
-    fn new(server: ForeignServer) -> Result<EtcdFdw, EtcdFdwError> {
+    fn new(server: ForeignServer) -> EtcdFdwResult<EtcdFdw> {
         // Open connection to etcd specified through the server parameter
         let rt = tokio::runtime::Runtime::new().unwrap();
-        let client = rt
-            .block_on(Client::connect(&[server.server_name], None))
-            .unwrap();
 
-        // Perform health checks and throw error on unhealthy
+        // Add parsing for the multi host connection string things here
+        let server_name = match server.options.get("connstr") {
+            Some(x) => x,
+            None => return Err(EtcdFdwError::NoConnStr(())),
+        };
 
-        Ok(Self { client, rt })
+        let client = match rt.block_on(Client::connect(&[server_name], None)) {
+            Ok(x) => x,
+            Err(e) => return Err(EtcdFdwError::ClientConnectionError(e.to_string())),
+        };
+        let prefix = match server.options.get("prefix") {
+            Some(x) => x.clone(),
+            None => String::from(""),
+        };
+        let fetch_results = vec![];
+
+        Ok(Self {
+            client,
+            rt,
+            prefix,
+            fetch_results,
+        })
     }
 
     fn begin_scan(
         &mut self,
-        quals: &[Qual],
-        columns: &[Column],
-        sorts: &[Sort],
+        _quals: &[Qual],
+        _columns: &[Column],
+        _sorts: &[Sort],
         limit: &Option<Limit>,
-        options: &std::collections::HashMap<String, String>,
+        _options: &std::collections::HashMap<String, String>,
     ) -> Result<(), EtcdFdwError> {
-        todo!()
+        // Select get all rows as a result into a field of the struct
+        // Build Query options from parameters
+        let mut get_options = GetOptions::new().with_all_keys();
+        match limit {
+            Some(x) => get_options = get_options.with_limit(x.count),
+            None => (),
+        }
+        // Also do quals, columns and sorts.
+
+        let result = self
+            .rt
+            .block_on(self.client.get(self.prefix.clone(), Some(get_options)));
+        let mut result_unwrapped = match result {
+            Ok(x) => x,
+            Err(e) => return Err(EtcdFdwError::FetchError(e.to_string())),
+        };
+        let result_vec = result_unwrapped.take_kvs();
+        self.fetch_results = result_vec;
+        Ok(())
     }
 
-    fn iter_scan(&mut self, row: &mut Row) -> Result<Option<()>, EtcdFdwError> {
-        todo!()
+    fn iter_scan(&mut self, row: &mut Row) -> EtcdFdwResult<Option<()>> {
+        // Go through results row by row and drain the result vector
+        if self.fetch_results.is_empty() {
+            Ok(None)
+        } else {
+            Ok(self.fetch_results.drain(0..1).last().map(|x| {
+                // Unpack x into a row
+                let key = x.key_str().expect("Expected a key, but the key was empty");
+                let value = x
+                    .value_str()
+                    .expect("Expected a value, but the value was empty");
+                row.push("key", Some(Cell::String(key.to_string())));
+                row.push("value", Some(Cell::String(value.to_string())));
+            }))
+        }
     }
 
-    fn end_scan(&mut self) -> Result<(), EtcdFdwError> {
-        todo!()
+    fn end_scan(&mut self) -> EtcdFdwResult<()> {
+        self.fetch_results = vec![];
+        Ok(())
     }
 
     fn begin_modify(
         &mut self,
         _options: &std::collections::HashMap<String, String>,
     ) -> Result<(), EtcdFdwError> {
-        todo!()
+        todo!("Begin modify is not yet implemented")
     }
 
     fn insert(&mut self, _row: &Row) -> Result<(), EtcdFdwError> {
-        todo!()
+        todo!("Insert is not yet implemented")
     }
 
     fn update(&mut self, _rowid: &Cell, _new_row: &Row) -> Result<(), EtcdFdwError> {
-        todo!()
+        todo!("Update is not yet implemented")
     }
 
     fn delete(&mut self, _rowid: &Cell) -> Result<(), EtcdFdwError> {
-        todo!()
+        todo!("Delete is not yet implemented")
     }
 
-    fn get_rel_size(
-        &mut self,
-        _quals: &[Qual],
-        _columns: &[Column],
-        _sorts: &[Sort],
-        _limit: &Option<Limit>,
-        _options: &std::collections::HashMap<String, String>,
-    ) -> Result<(i64, i32), EtcdFdwError> {
-        todo!()
-    }
+    // fn get_rel_size(
+    //     &mut self,
+    //     _quals: &[Qual],
+    //     _columns: &[Column],
+    //     _sorts: &[Sort],
+    //     _limit: &Option<Limit>,
+    //     _options: &std::collections::HashMap<String, String>,
+    // ) -> Result<(i64, i32), EtcdFdwError> {
+    //     todo!("Get rel size is not yet implemented")
+    // }
 
     fn end_modify(&mut self) -> Result<(), EtcdFdwError> {
         todo!()
